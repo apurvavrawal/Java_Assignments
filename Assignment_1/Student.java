8a24110f01f6e43bf05adb5688308ada5d122c63
apurvavrawal <apurva.rawal@joshsoftware.com>
2023-12-12 12:42:01 +0530

    Suggested changes for Assignments-1,2,3,4,7 are added

--- Assignment_1/Student.java
+++ Assignment_1/Student.java
@@ -1,15 +1,15 @@
 public class Student extends Person{
-    private int roll_no;
+    private int rollNo;
 
     private String course;
     private String department;
 
     public int getRoll_no() {
-        return roll_no;
+        return rollNo;
     }
 
-    public void setRoll_no(int roll_no) {
-        this.roll_no = roll_no;
+    public void setRoll_no(int rollNo) {
+        this.rollNo = rollNo;
     }
 
     public String getCourse() {
@@ -28,9 +28,9 @@ public class Student extends Person{
         this.department = department;
     }
 
-    public Student(String name, int age, int roll_no, String course, String department) {
+    public Student(String name, int age, int rollNo, String course, String department) {
         super(name, age);
-        this.roll_no = roll_no;
+        this.rollNo = rollNo;
         this.course = course;
         this.department = department;
     }
---
+++ Assignment_7/DTOToJSONConversion/src/main/java/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+
+    private String firstName;
+    private String lastname;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
